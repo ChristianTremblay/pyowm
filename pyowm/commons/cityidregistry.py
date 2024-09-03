145c733c19ec1980cf63b880e0b1ca83a7a3935e
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-09-03 00:38:48 -0400

    fix windows SQL bug change ROOT_WEATHER_API Converted to pyproject.toml so pip install work

--- pyowm/commons/cityidregistry.py
+++ pyowm/commons/cityidregistry.py
@@ -2,19 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import bz2
+import importlib.resources as resources
+import os
 import sqlite3
 import tempfile
-from pkg_resources import resource_filename
+from pathlib import Path
+
 from pyowm.weatherapi25.location import Location
 
-CITY_ID_DB_PATH = 'cityids/cities.db.bz2'
+CITY_ID_DB_PATH = "cityids/cities.db.bz2"
 
 
 class CityIDRegistry:
 
     MATCHINGS = {
-        'exact': "SELECT city_id, name, country, state, lat, lon FROM city WHERE name=?",
-        'like': r"SELECT city_id, name, country, state, lat, lon FROM city WHERE name LIKE ?"
+        "exact": "SELECT city_id, name, country, state, lat, lon FROM city WHERE name=?",
+        "like": r"SELECT city_id, name, country, state, lat, lon FROM city WHERE name LIKE ?",
     }
 
     def __init__(self, sqlite_db_path: str):
@@ -39,21 +42,26 @@ class CityIDRegistry:
         # https://pymotw.com/2/bz2/
 
         # read and uncompress data from compressed DB
-        res_name = resource_filename(__name__, sqlite_db_path)
-        bz2_db = bz2.BZ2File(res_name)
-        decompressed_data = bz2_db.read()
+        subfolder, filename = os.path.split(sqlite_db_path)
+        module_name = ".".join(__name__.split(".")[:-1])
+        with resources.path(f"{module_name}.{subfolder}", filename) as bz2_db_path:
+            with open(bz2_db_path, "rb") as bz2_db:
+                decompressed_data = bz2.BZ2File(bz2_db).read()
 
         # dump decompressed data to a temp DB
-        with tempfile.NamedTemporaryFile(mode='wb') as tmpf:
-            tmpf.write(decompressed_data)
-            tmpf_name = tmpf.name
+        try:
+            with tempfile.NamedTemporaryFile(mode="wb", delete=False) as tmpf:
+                tmpf.write(decompressed_data)
+                tmpf_name = tmpf.name
 
-            # read temp DB to memory and return handle
-            src_conn = sqlite3.connect(tmpf_name)
-            dest_conn = sqlite3.connect(':memory:')
-            src_conn.backup(dest_conn)
-            src_conn.close()
-            return dest_conn
+                # read temp DB to memory and return handle
+                src_conn = sqlite3.connect(tmpf_name)
+                dest_conn = sqlite3.connect(":memory:")
+                src_conn.backup(dest_conn)
+                src_conn.close()
+                return dest_conn
+        except Exception as e:
+            os.remove(tmpf_name)
 
     def __query(self, sql_query: str, *args):
         """
@@ -67,7 +75,7 @@ class CityIDRegistry:
         finally:
             cursor.close()
 
-    def ids_for(self, city_name, country=None, state=None, matching='like'):
+    def ids_for(self, city_name, country=None, state=None, matching="like"):
         """
         Returns a list of tuples in the form (city_id, name, country, state, lat, lon )
         The rule for querying follows the provided `matching` parameter value.
@@ -89,31 +97,35 @@ class CityIDRegistry:
         if not city_name:
             return []
         if matching not in self.MATCHINGS:
-            raise ValueError("Unknown type of matching: "
-                             "allowed values are %s" % ", ".join(self.MATCHINGS))
+            raise ValueError(
+                "Unknown type of matching: "
+                "allowed values are %s" % ", ".join(self.MATCHINGS)
+            )
         if country is not None and len(country) != 2:
             raise ValueError("Country must be a 2-char string")
         if state is not None and country is None:
-            raise ValueError("A country must be specified whenever a state is specified too")
+            raise ValueError(
+                "A country must be specified whenever a state is specified too"
+            )
 
         q = self.MATCHINGS[matching]
-        if matching == 'exact':
+        if matching == "exact":
             params = [city_name]
         else:
-            params = ['%' + city_name + '%']
+            params = ["%" + city_name + "%"]
 
         if country is not None:
-            q = q + ' AND country=?'
+            q = q + " AND country=?"
             params.append(country)
 
         if state is not None:
-            q = q + ' AND state=?'
+            q = q + " AND state=?"
             params.append(state)
 
         rows = self.__query(q, *params)
         return rows
 
-    def locations_for(self, city_name, country=None, state=None, matching='like'):
+    def locations_for(self, city_name, country=None, state=None, matching="like"):
         """
         Returns a list of `Location` objects
         The rule for querying follows the provided `matching` parameter value.
@@ -133,9 +145,12 @@ class CityIDRegistry:
         :return: list of `Location` objects
         """
         items = self.ids_for(city_name, country=country, state=state, matching=matching)
-        return [Location(item[1], item[5], item[4], item[0], country=item[2]) for item in items]
+        return [
+            Location(item[1], item[5], item[4], item[0], country=item[2])
+            for item in items
+        ]
 
-    def geopoints_for(self, city_name, country=None, state=None, matching='like'):
+    def geopoints_for(self, city_name, country=None, state=None, matching="like"):
         """
         Returns a list of ``pyowm.utils.geo.Point`` objects corresponding to
         the int IDs and relative toponyms and 2-chars country of the cities
@@ -157,5 +172,7 @@ class CityIDRegistry:
         :raises ValueError if the value for `matching` is unknown
         :return: list of `pyowm.utils.geo.Point` objects
         """
-        locations = self.locations_for(city_name, country=country, state=state, matching=matching)
+        locations = self.locations_for(
+            city_name, country=country, state=state, matching=matching
+        )
         return [loc.to_geopoint() for loc in locations]
